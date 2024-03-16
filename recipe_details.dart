d249de71bf3257cb77e4011a7d7afbce4ac18220
Navya Saju <83907445+navya-1231@users.noreply.github.com>
2024-03-16 09:07:38 +0530

    Add files via upload

--- recipe_details.dart
+++ recipe_details.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fraction/fraction.dart';
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -23,7 +24,7 @@ class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
   late TabViewWidget _tabViewWidget;
   late TextEditingController _commentController;
 
-  int _servings = 1;
+  int _servings = 0;
   String _userEmail = '';
   bool _isLiked = false;
   bool _isSaved = false;
@@ -39,6 +40,7 @@ class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
     _getUserEmail(); 
     _loadLikeStatus();
     _loadSaveStatus();
+  _servings = widget.recipeSnapshot['serving'] ?? 1; 
     _loadCheckboxStates(); 
   }
 
@@ -70,8 +72,9 @@ class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
       _isSaved = !_isSaved;
     });
 
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool(widget.recipeSnapshot.id, _isSaved);
+   
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setBool(widget.recipeSnapshot.id, _isSaved);
 
     String userId = _userEmail;
 
@@ -104,24 +107,18 @@ void toggleCheckbox(int index) {
 
 void _saveCheckboxStates() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  // Convert boolean values to strings
-  Map<String, String> stringMap = _isChecked.map((key, value) => MapEntry(key.toString(), value.toString()));
-  prefs.setString('checkboxStates', jsonEncode(stringMap));
+  prefs.setString('checkboxStates', jsonEncode(_isChecked));
 }
-
 void _loadCheckboxStates() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String? checkboxStatesJson = prefs.getString('checkboxStates');
   if (checkboxStatesJson != null && checkboxStatesJson.isNotEmpty) {
     Map<String, dynamic> decodedMap = jsonDecode(checkboxStatesJson);
-    // Convert string values back to boolean
     setState(() {
-      _isChecked = decodedMap.map((key, value) => MapEntry(int.parse(key), value == 'true'));
+      _isChecked = decodedMap.map((key, value) => MapEntry(int.parse(key), value));
     });
   }
 }
-
-
   @override
   Widget build(BuildContext context) {
     _tabViewWidget = TabViewWidget(
@@ -138,12 +135,15 @@ void _loadCheckboxStates() async {
     return Scaffold(
       backgroundColor: Color(0xFFD1E7D2),
       body: ListView(
+        
         children: [
+           
           _videoWidget,
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Row(
               children: [
+                
                 Expanded(
                   child: Text(
                     widget.recipeSnapshot['title'], // Display recipe title here
@@ -215,6 +215,8 @@ class _VideoWidgetState extends State<VideoWidget> {
   late VideoPlayerController _videoPlayerController;
   late ChewieController _chewieController;
   bool _isVideoLoading = true;
+  
+
 
   @override
   void initState() {
@@ -222,7 +224,7 @@ class _VideoWidgetState extends State<VideoWidget> {
     _initializeVideo();
   }
 
-  @override
+ @override
   void dispose() {
     _videoPlayerController.dispose();
     _chewieController.dispose();
@@ -240,9 +242,10 @@ class _VideoWidgetState extends State<VideoWidget> {
         videoPlayerController: _videoPlayerController,
         autoPlay: true,
         looping: true,
-        allowFullScreen: true, // Adjust according to your needs
+        allowFullScreen: true, 
         aspectRatio: 4 / 3,
-        // You can customize more options here
+        useRootNavigator: true,
+        
       );
       _videoPlayerController.addListener(() {
         if (_videoPlayerController.value.hasError) {
@@ -264,25 +267,22 @@ class _VideoWidgetState extends State<VideoWidget> {
     final url = await ref.getDownloadURL();
     return url;
   }
-
  @override
-Widget build(BuildContext context) {
-  return _isVideoLoading
-      ? AspectRatio(
-          aspectRatio: 4 / 3, // Set a default aspect ratio while loading
-          child: Center(child: CircularProgressIndicator()),
-        )
-      : _chewieController != null
-          ? AspectRatio(
-              aspectRatio: _chewieController.aspectRatio ?? 16 / 9,
-              child: Chewie(
-                controller: _chewieController,
-              ),
-            )
-          : Container(); // Return an empty container if chewieController is not initialized yet
+  Widget build(BuildContext context) {
+    return _isVideoLoading
+        ? AspectRatio(
+            aspectRatio: 4 / 3, // Set a default aspect ratio while loading
+            child: Center(child: CircularProgressIndicator()),
+          )
+        : AspectRatio(
+            aspectRatio: _chewieController.aspectRatio ?? 16 / 9,
+            child: Chewie(
+              controller: _chewieController,
+            ),
+          );
+  }
 }
 
-}
 
 class UploaderDetailsWidget extends StatelessWidget {
   final DocumentSnapshot recipeSnapshot;
@@ -323,7 +323,7 @@ class UploaderDetailsWidget extends StatelessWidget {
                 if (urlSnapshot.connectionState == ConnectionState.waiting) {
                   return CircularProgressIndicator(
                     valueColor:
-                        AlwaysStoppedAnimation<Color>(const Color(0xFFD1E7D2)),
+                        AlwaysStoppedAnimation<Color>(Color(0xFFD1E7D2)),
                     strokeWidth: 2.0,
                   );
                 } else if (urlSnapshot.hasError) {
@@ -403,7 +403,7 @@ class TabViewWidget extends StatelessWidget {
             height: 400,
             child: TabBarView(
               children: [
-                _buildIngredientsTab(servings),
+                _buildIngredientsTab(),
                 _buildStartCookTab(),
                 _buildCommentsTab(context),
               ],
@@ -413,8 +413,7 @@ class TabViewWidget extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildIngredientsTab(int servings) {
+Widget _buildIngredientsTab() {
   final recipeSnapshot = this.recipeSnapshot;
   return SingleChildScrollView(
     child: Padding(
@@ -441,6 +440,7 @@ class TabViewWidget extends StatelessWidget {
 
               List<QueryDocumentSnapshot> ingredients = snapshot.data!.docs;
               int totalIngredients = ingredients.length;
+              
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -482,11 +482,11 @@ class TabViewWidget extends StatelessWidget {
                     itemCount: ingredients.length,
                     itemBuilder: (context, index) {
                       String name = ingredients[index].id;
-                      String originalQuantity = ingredients[index]['quantity'];
-                      String adjustedQuantity =
-                          _calculateAdjustedQuantity(originalQuantity);
+                      String quantity = ingredients[index]['quantity'];
+                      String unit = _getUnit(ingredients[index]);                      
+                      String adjustedQuantity = _calculateAdjustedQuantity(quantity, servings);
                       return ListTile(
-                        title: Text('$name: $adjustedQuantity'),
+                        title: Text('$name : $adjustedQuantity $unit  '),
                         trailing: Checkbox(
                           value: isChecked[index] ?? false,
                           onChanged: (value) {
@@ -494,7 +494,7 @@ class TabViewWidget extends StatelessWidget {
                             if (value == true) {
                               // Add ingredient to selectedIngredients in Firestore
                               _addIngredientToSelectedIngredients(name);
-                            }else {
+                            } else {
                               // Remove ingredient from selectedIngredients in Firestore
                               _removeIngredientFromSelectedIngredients(name);
                             }
@@ -513,7 +513,70 @@ class TabViewWidget extends StatelessWidget {
     ),
   );
 }
+String _calculateAdjustedQuantity(String originalQuantity, int servings) {
+   
+   Fraction originalFraction = _parseQuantity(originalQuantity);
 
+   MixedFraction adjustedFraction = (originalFraction * Fraction(servings, 1)).toMixedFraction();
+  if (adjustedFraction.numerator == 0 || adjustedFraction.denominator == 1 ) {
+       
+        return adjustedFraction.toFraction().toString();
+    } else {
+       
+        return adjustedFraction.toString();
+   }
+}
+
+
+Fraction _parseQuantity(String quantity) {
+  try {
+    // Check if the quantity contains a space, indicating a mixed fraction
+    if (quantity.contains(' ')) {
+      // Split the mixed fraction into whole number, numerator, and denominator
+      List<String> parts = quantity.split(' ');
+      int wholeNumber = int.parse(parts[0]);
+      String fraction = parts[1];
+
+      // Split the fraction into numerator and denominator
+      List<String> fractionParts = fraction.split('/');
+      int numerator = int.parse(fractionParts[0]);
+      int denominator = int.parse(fractionParts[1]);
+
+      // Calculate the equivalent improper fraction
+      int improperNumerator = wholeNumber * denominator + numerator;
+
+      // Create and return the mixed fraction
+      return Fraction(improperNumerator, denominator);
+    }
+
+    // Check if the quantity contains a slash, indicating a fraction
+    else if (quantity.contains('/')) {
+      // Split the fraction into numerator and denominator
+      List<String> parts = quantity.split('/');
+      int numerator = int.parse(parts[0]);
+      int denominator = int.parse(parts[1]);
+
+      // Create and return the fraction
+      return Fraction(numerator, denominator);
+    }
+
+    // If the quantity is a whole number, parse it as an integer
+    else {
+      return Fraction.fromString(quantity);
+    }
+  } catch (e) {
+    print('Error parsing quantity: $e');
+    // Return a default fraction if an error occurs during parsing
+    return Fraction(0, 1);
+  }
+}
+
+
+
+String _getUnit(QueryDocumentSnapshot ingredient) {
+  final data = ingredient.data() as Map<String, dynamic>?; 
+  return data != null && data.containsKey('unit') ? data['unit'] as String : '';
+}
 void _addIngredientToSelectedIngredients(String ingredientName) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String? userId = prefs.getString('email');
@@ -526,6 +589,7 @@ void _addIngredientToSelectedIngredients(String ingredientName) async {
     });
   }
 }
+
 
 void _removeIngredientFromSelectedIngredients(String ingredientName) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -541,82 +605,87 @@ void _removeIngredientFromSelectedIngredients(String ingredientName) async {
 }
 
 
-  String _calculateAdjustedQuantity(String originalQuantity) {
-    double originalQuantityValue = double.tryParse(originalQuantity) ?? 0.0;
-    double adjustedQuantityValue = originalQuantityValue * servings;
-    return adjustedQuantityValue.toStringAsFixed(2);
-  }
 
-  Widget _buildStartCookTab() {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance
-          .collection('recipe')
-          .doc(recipeSnapshot.id)
-          .collection('steps')
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(const Color(0xFFD1E7D2)),
-            strokeWidth: 2.0,
-          );
-        }
-        if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Text('No steps found');
-        }
-
-        List<QueryDocumentSnapshot> steps = snapshot.data!.docs;
-        if (currentPageIndex >= steps.length) {
-          updateCurrentPageIndex(steps.length - 1);
-        }
-
-        String currentStep =
-            steps.isNotEmpty ? steps[currentPageIndex]['description'] : '';
-
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Step ${currentPageIndex + 1}:',
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                currentStep,
-                style: TextStyle(fontSize: 18.0),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      if (currentPageIndex > 0) {
-                        updateCurrentPageIndex(currentPageIndex - 1);
-                      }
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.arrow_forward),
-                    onPressed: () {
-                      if (currentPageIndex < steps.length - 1) {
-                        updateCurrentPageIndex(currentPageIndex + 1);
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
+Widget _buildStartCookTab() {
+  return StreamBuilder<QuerySnapshot>(
+    stream: FirebaseFirestore.instance
+        .collection('recipe')
+        .doc(recipeSnapshot.id)
+        .collection('steps')
+        .snapshots(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(const Color(0xFFD1E7D2)),
+          strokeWidth: 2.0,
         );
-      },
-    );
-  }
+      }
+      if (snapshot.hasError) {
+        return Text('Error: ${snapshot.error}');
+      }
+      if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+        return Text('No steps found');
+      }
+
+      List<QueryDocumentSnapshot> steps = snapshot.data!.docs;
+      if (currentPageIndex >= steps.length) {
+        updateCurrentPageIndex(steps.length - 1);
+      }
+
+      String currentStep =
+          steps.isNotEmpty ? steps[currentPageIndex]['description'] : '';
+
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Step ${currentPageIndex + 1}:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: () {
+                    if (currentPageIndex > 0) {
+                      updateCurrentPageIndex(currentPageIndex - 1);
+                    }
+                  },
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFD1E7D2)),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Text(
+                      currentStep,
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward_ios),
+                  onPressed: () {
+                    if (currentPageIndex < steps.length - 1) {
+                      updateCurrentPageIndex(currentPageIndex + 1);
+                    }
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+
 
   Widget _buildCommentsTab(BuildContext context) {
     return Column(
